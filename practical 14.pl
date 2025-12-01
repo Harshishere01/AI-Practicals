ec7331ad58ac6f5dbe0a497130fc61386432c332
Harsh Tanwar <harshtanwarfreefire@gmail.com>
2025-12-01 22:24:12 +0530

    Update practical 14.pl

--- practical 14.pl
+++ practical 14.pl
@@ -1,3 +1,6 @@
+14. Write a PROLOG program to implement insert(I, N, L, R) that inserts an item I into
+Nth position of list L to generate a list R.
+
 % Case 1: Insert at position 1 (front of the list)
 insert(I, 1, L, [I|L]).
 
@@ -6,3 +9,4 @@ insert(I, N, [H|T], [H|R]) :-
     N > 1,
     N1 is N - 1,
     insert(I, N1, T, R).
+
