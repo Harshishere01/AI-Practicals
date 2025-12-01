548a66aac55c1e1faac98b6f7323f6cbe47be8ec
Harsh Tanwar <harshtanwarfreefire@gmail.com>
2025-12-01 22:21:08 +0530

    Refactor reverse function implementation in Prolog

--- Practical 3.pl
+++ Practical 3.pl
@@ -1,3 +1,6 @@
+3. Write a PROLOG program to implement reverse(L, R) where List L is original and List
+R is reversed list
+
 %reverse(L,R):- R is the reverse of the list L
 reverse(L,R):-
     reverse_acc(L,[],R).
@@ -7,3 +10,4 @@ reverse_acc([],Acc,Acc).
 
 reverse_acc([H|T],Acc,R):-
     reverse_acc(T,[H|Acc],R).
+
