0f769fc9917a479c4ee0f6d50243ab80430168bc
Harsh Tanwar <harshtanwarfreefire@gmail.com>
2025-12-01 22:20:47 +0530

    Implement conc/3 for list concatenation in PROLOG
    Added a description for the PROLOG program that implements list concatenation.

--- practical 2.pl
+++ practical 2.pl
@@ -1,3 +1,6 @@
+2. Write a PROLOG program to implement conc(L1, L2, L3) where L2 is the list to be
+appended with L1 to get the resulted list L3.
+
 %Base case: concatenating an empty list with L yeilds L
 conc([],L,L).
 
@@ -5,4 +8,5 @@ conc([],L,L).
 %and recursively concatenating the tail with L2
 conc([H|T],L2,[H|R]):-
     conc(T,L2,R).
+
 
