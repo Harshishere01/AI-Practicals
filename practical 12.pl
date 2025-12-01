b5d3e0f20218af78d0a3ea93217d085d53a31148
Harsh Tanwar <harshtanwarfreefire@gmail.com>
2025-12-01 22:23:38 +0530

    Add evenlength and oddlength predicates in Prolog
    Implemented two predicates to check if a list has even or odd length.

--- practical 12.pl
+++ practical 12.pl
@@ -1,3 +1,7 @@
+12. Write a PROLOG program to implement two predicates evenlength(List) and
+oddlength(List) so that they are true if their argument is a list of even or odd length
+respectively
+
 % A list of length 0 is even
 evenlength([]).
 
@@ -11,3 +15,4 @@ oddlength([_]).
 % If a list has 2 elements more, its parity stays odd
 oddlength([_,_|T]) :-
     oddlength(T).
+
