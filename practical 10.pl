18be27d9a7093248c424af0dec725a338ab3dec6
Harsh Tanwar <harshtanwarfreefire@gmail.com>
2025-12-01 22:23:12 +0530

    Refactor memb predicate in practical 10.pl

--- practical 10.pl
+++ practical 10.pl
@@ -1,6 +1,10 @@
+10. Write a PROLOG program to implement memb(X, L): to check whether X is a member
+of L or not.
+
 % Case 1: X is the head of the list
 memb(X, [X|_]).
 
 % Case 2: X is in the tail of the list
 memb(X, [_|T]) :-
     memb(X, T).
+
